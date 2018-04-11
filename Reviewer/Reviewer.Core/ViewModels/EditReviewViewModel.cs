79234eb4828322cbe53413fa3fc53c95201f3794
Matt Soucoup <msoucoup@codemilltech.com>
2018-04-11 14:56:22 -0500

    Edit review scaffold

--- Reviewer/Reviewer.Core/ViewModels/EditReviewViewModel.cs
+++ Reviewer/Reviewer.Core/ViewModels/EditReviewViewModel.cs
@@ -7,11 +7,19 @@ namespace Reviewer.Core
         Review review;
         public Review Review { get => review; set => SetProperty(ref review, value); }
 
-        public EditReviewViewModel(Review theReview)
+        Business business;
+        public Business Business { get => business; set => SetProperty(ref business, value); }
+
+        public EditReviewViewModel(Review theReview, Business theBusiness)
         {
             Review = theReview;
 
             Title = "A Review";
+        }
+
+        public EditReviewViewModel(Business theBusiness) : this(null, theBusiness)
+        {
+
         }
     }
 }
