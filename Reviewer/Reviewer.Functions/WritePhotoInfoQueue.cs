4b5bd549d053bf1efd57edd3dd862015653ddd12
Matt Soucoup <msoucoup@codemilltech.com>
2018-04-24 15:45:50 -0500

    Writing to cosmos!

--- Reviewer/Reviewer.Functions/WritePhotoInfoQueue.cs
+++ Reviewer/Reviewer.Functions/WritePhotoInfoQueue.cs
@@ -14,54 +14,16 @@ namespace Reviewer.Functions
     {
         [FunctionName("WritePhotoInfoQueue")]
         [return: Queue("review-photos")]
-        public static QueueMsg Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]QueueMsg req, TraceWriter log)
+        public static QueueMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]QueueMessage req, TraceWriter log)
         {
-
             log.Info("Starting the queue trigger");
             log.Info($"Review ID: {req.reviewId}, Photo Url: {req.photoUrl}");
-
-            // Get request body
-
-            // Set name to query string or body data
-            //name = name ?? data?.name;
-
-            //return name == null
-            //? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-            //: req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
-
-            //return req.CreateErrorResponse
 
             return req;
         }
 
-        //[FunctionName("WritePhotoInfoCosmos")]
-        //public static void Run([QueueTrigger("review-photos")]string myQueueItem,
-        //    [DocumentDB("build", "reviews", ConnectionStringSetting = "Reviews_Cosmos", Id = "/id")]out dynamic document,
-        //    TraceWriter log)
-        //{
-        //    log.Info($"C# Queue trigger function processed: {myQueueItem}");
-
-        //    try
-        //    {
-        //        var jsonified = JsonConvert.DeserializeObject<PhotoInfo>(myQueueItem);
-
-        //        if (jsonified != null)
-        //            document = jsonified;
-        //        else
-        //            document = null;
-        //    }
-        //    catch (JsonReaderException ex)
-        //    {
-        //        log.Error($"*** Error: {ex.Message}");
-        //        document = null;
-        //    }
-        //}:w
-
+        
     }
 
-    public class QueueMsg
-    {
-        public string reviewId { get; set; }
-        public string photoUrl { get; set; }
-    }
+    
 }
